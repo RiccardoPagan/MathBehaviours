cfea3e1b84c76adc1b411ad99ccdd47a3e1207ac
Riccardo <pagan.riccardo17@gmail.com>
2025-11-30 23:43:15 +0100

    Updated for the new hp

--- Script/Analysis_Behaviours.R
+++ Script/Analysis_Behaviours.R
@@ -1,6 +1,5 @@
 # empty workspace
-rm(list=ls())
-gc()
+rm(list=ls()); gc()
 
 #load required packages
 library(brms)
@@ -9,8 +8,11 @@ library(lme4)
 library(effects)
 #library(ordinal)
 
+Path <- getwd()
+
 # import dataset
 d <- read.csv("Data/SimulatedDatasetBehaviours.csv", sep = ",")
+
 
 # data preparation
 d$choice = paste0(d$skip,d$help,d$self)
@@ -24,94 +26,130 @@ d$choice = case_when(
 ########################################################################
 # RQ1: Math behaviours are ordered or categorical?
 ########################################################################
+d <- subset(d, session == 2)
 
-my_formula <- choice ~ buoyancy + avoidance + worry + pc_state + math_anxiety + pc_trait + math_ability + Gender + (1|id)
+my_formula_cat <- choice_c ~ buoyancy + avoidance + worry + sc_state + math_anxiety + sc_trait + math_ability_T0 + Gender + (1|id)
+#my_formula_cat <- choice_c ~ math_anxiety + (1|id)
+d$choice_c  <- factor(d$choice,
+                      levels = c("skip", "help", "self"))
+
+my_formula_ord <- choice_ord ~ buoyancy + avoidance + worry + sc_state + math_anxiety + sc_trait + math_ability_T0 + Gender + (1|id)
+#my_formula_ord <- choice_ord ~ math_anxiety + (1|id)
+d$choice_ord <- ordered(d$choice, 
+                        levels = c("skip", "help", "self"))
 
 # model with behaviours as categorical variable
 fit_cat <- brm(
-  formula = my_formula,
+  formula = my_formula_cat,
   data = d,
   family = categorical(link = "logit"),
   cores = 4
 )
 
-
 # model with choice as ordered (skip < help < self)
-d$choice <- factor(d$choice, 
-                   levels = c("skip", "help", "self"), 
-                   ordered = TRUE)
-
-# model with behaviours as ordered variable
 fit_ord <- brm(
-  formula = my_formula,
+  formula = my_formula_ord,
   data = d,
   family = cumulative(link = "logit"),
   cores = 4
 )
 
 
-
 # model comparison
-comparison1 <- loo(fit_ord, fit_cat)
+loo_cat <- loo(fit_cat)
+loo_ord <- loo(fit_ord)
+comparison1 <- loo_compare(loo_cat, loo_ord)
 
-loo_cat = loo(fit_cat)
-loo_ord = loo(fit_ord)
-
-comparison2 <- loo_compare(loo_cat, loo_ord)
-
-
-
+waic_cat <- waic(fit_cat)
+waic_ord <- waic(fit_ord)
+comparison2 <- loo_compare(waic_cat, waic_ord)
 
 
+# output results
+setwd(paste0(Path, "/outputs"))
+
+sink("output.txt")
+cat("=========================================\n")
+cat("Categorical model\n")
+cat("=========================================\n\n")
+summary(fit_cat)
+
+cat("\n\n")
+
+cat("=========================================\n")
+cat("Ordered model\n")
+cat("=========================================\n\n")
+summary(fit_ord)
+sink()
+closeAllConnections()
+
+
+sink("LOO.txt")
+cat("=========================================\n")
+cat("LOO categorical model\n")
+cat("=========================================\n\n")
+loo_cat
+
+cat("\n\n")
+
+cat("=========================================\n")
+cat("LOO ordered model\n")
+cat("=========================================\n\n")
+loo_ord
+
+sink()
+closeAllConnections()
+
+
+sink("WAIC.txt")
+cat("=========================================\n")
+cat("WAIC categorical model\n")
+cat("=========================================\n\n")
+waic_cat
+
+cat("\n\n")
+
+cat("=========================================\n")
+cat("WAIC ordered model\n")
+cat("=========================================\n\n")
+waic_ord
+
+sink()
+closeAllConnections()
 
 
 
+sink("comparison.txt")
+cat("=========================================\n")
+cat("LOO COMPARISON\n")
+cat("=========================================\n\n")
+comparison1
 
+cat("\n\n")
 
+cat("=========================================\n")
+cat("WAIC COMPARISON\n")
+cat("=========================================\n\n")
+comparison2
 
-
-
-
-
-#model with choice as categorical variable
-
-fit1 <- brm(choice ~ math_anxiety + buoyancy + avoidance + pc + worry +
-              metacognition + math_ability + (1|ID), data=d,
-            family=categorical(), cores=4)
-
-summary(fit1)
-plot(fit1)
-
+sink()
+closeAllConnections()
 
 ########################################################################
-# RQ2: Math behaviours as predictor of math performance
+# RQ2: State and trait measures as predictor of math behaviours
 ########################################################################
 
-d <- subset(d, session == 2)
-d_ns <- subset(d, choice != "skip")
-
-# model without skip
-fit2 <- glmer(
-  accuracy ~ choice + math_ability + (1|ID),
-  data = d_ns,
-  family = binomial(link = "logit")
-)
-
-summary(fit2)
-plot(allEffects(fit2),multiline=T,ci.style="auto")
-
-
-# bayesan model to try to stabilize the skip coefficient with a prior (not the ideal solution)
-fit_bay <- brm(
-  accuracy ~ choice + math_ability + (1 | ID),
+# model with behaviours as categorical variable (apparently best model)
+fit_cat <- brm(
+  formula = my_formula_cat,
   data = d,
-  family = bernoulli(link = "logit"),
-  prior = prior(normal(0, 1), class = "b"), 
-  cores = 4, chains = 4
+  family = categorical(link = "logit"),
+  cores = 4
 )
 
-summary(fit_bay)
-plot(fit_bay)
+summary(fit_cat)
+plot(fit_cat)
+
 
 
 
